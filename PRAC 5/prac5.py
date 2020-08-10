2697d4c4adfad1c9aff0c12ecc81162b343c8713
sahiljanbandhu <sahil14498@gmail.com>
2020-08-10 15:51:24 +0530

    prac 5 added

---
+++ PRAC 5/prac5.py
@@ -0,0 +1,13 @@
+"""
+import numpy as np
+def(A):
+    return sum([(-)**;*A[i,0]* def(np.delete(np.delete(A,0,1),i,0])) for i in range (A.shape[0])]) if(A.shape!=(1,1) else A[0,0])
+m=np.array([4,3,0],[44,55,66],[74,85,96]]
+print(def(m))
+
+"""
+
+import numpy as np
+m=np.matrix[[4,5,6],[44,55,66],[74,85,96]]
+m.I
+print(m.I)
